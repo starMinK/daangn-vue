8db5f1c3921fb94f3eed0c6034f61a680f536f28
김규민 <106909332+starMinK@users.noreply.github.com>
2023-12-02 01:58:23 +0900

    20231202

--- daangnClone/src/App.vue
+++ daangnClone/src/App.vue
@@ -1,65 +1,24 @@
+<script>
+import Header from "./components/Header.vue"
+import Footer from "./components/Footer.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+
+  }
+}
+</script>
+
+
 <template>
   <div id="gnb-root">
     <div class="light-theme">
-      <div class="_1knjz490">
-        <div class="_1knjz491 _1s38h9c0"><a class="_1knjz492" href="https://www.daangn.com"><span><svg width="65"
-                                                                                                       height="36"
-                                                                                                       viewBox="0 0 65 36"
-                                                                                                       fill="none"
-                                                                                                       xmlns="http://www.w3.org/2000/svg"><g
-            clip-path="url(#clip0_47_13130)"><path
-            d="M9.23186 11.6511C4.1335 11.6511 0 15.6932 0 20.7962C0 27.847 9.25838 31.5884 9.23186 31.5783C9.20661 31.5884 18.4637 27.847 18.4637 20.7962C18.4637 15.6969 14.3302 11.6511 9.23186 11.6511ZM9.23186 24.4985C8.54053 24.498 7.86486 24.2924 7.2903 23.9079C6.71573 23.5233 6.26807 22.977 6.00391 22.338C5.73975 21.6991 5.67096 20.9961 5.80623 20.318C5.9415 19.6399 6.27476 19.0171 6.76388 18.5285C7.25299 18.0398 7.87599 17.7072 8.55411 17.5726C9.23223 17.438 9.93502 17.5076 10.5736 17.7725C11.2122 18.0374 11.7579 18.4857 12.1418 19.0608C12.5256 19.6359 12.7304 20.3119 12.7301 21.0034C12.731 21.4632 12.6411 21.9187 12.4656 22.3437C12.2901 22.7687 12.0325 23.1549 11.7075 23.4802C11.3825 23.8055 10.9966 24.0634 10.5718 24.2392C10.1469 24.415 9.69161 24.5053 9.23186 24.5048V24.4985Z"
-            fill="#FF6F0F"></path><path
-            d="M11.3103 0C9.15573 0 7.6554 1.50565 7.41923 3.28415C4.54738 2.4909 2.29688 4.67359 2.29688 7.07354C2.29688 8.9114 3.55978 10.3956 5.24324 10.8781C6.60087 11.2659 9.04586 10.9766 9.04586 10.9766C9.03323 10.383 9.58007 9.73118 10.4237 9.13751C12.8232 7.44996 14.6999 6.65419 14.9891 4.35276C15.2922 1.94523 13.5052 0 11.3103 0Z"
-            fill="#00A05B"></path><path
-            d="M36.7873 14.8305C34.8374 15.4431 31.4616 15.769 28.2905 15.769V11.9114H34.8399V9.03906H24.7783V18.8208C30.2808 18.8208 35.0849 18.1576 37.1132 17.5298L36.7873 14.8305Z"
-            fill="#FF6F0F"></path><path
-            d="M41.4104 7.89453H37.9033V19.1402H41.4104V15.6148H44.2393V12.5467H41.4104V7.89453Z" fill="#FF6F0F"></path><path
-            d="M34.8969 19.1138C31.1814 19.1138 28.4561 21.1613 28.4561 24.1663C28.4561 27.1713 31.1839 29.2188 34.8969 29.2188C38.6098 29.2188 41.3377 27.17 41.3377 24.1663C41.3377 21.1626 38.6124 19.1138 34.8969 19.1138ZM34.8969 26.4058C33.2223 26.4058 31.9657 25.6025 31.9657 24.1613C31.9657 22.72 33.2223 21.9179 34.8969 21.9179C36.5715 21.9179 37.8281 22.7213 37.8281 24.1613C37.8281 25.6012 36.5728 26.4058 34.8969 26.4058Z"
-            fill="#FF6F0F"></path><path d="M51.0698 20.6055H47.4844V28.2133H61.735V25.202H51.0698V20.6055Z"
-                                        fill="#FF6F0F"></path><path
-            d="M61.2902 8.83301H47.3224V11.8443H57.7692C57.7692 12.5921 57.8046 14.3706 57.4623 16.3916H44.6514V19.5065H64.1002V16.3966H60.6347C61.0995 13.8426 61.2321 11.8519 61.2902 8.83301Z"
-            fill="#FF6F0F"></path></g><defs><clipPath id="clip0_47_13130"><rect width="64.1053" height="36"
-                                                                                fill="white"></rect></clipPath></defs></svg></span></a>
-          <nav class="_1knjz49j _1s38h9c0">
-            <ul class="_1knjz49l _1s38h9c5">
-              <li class="_1knjz49n"><a class="_1knjz49o _1s38h9c4 _1s38h9c2 undefined"
-                                       href="https://www.daangn.com/fleamarket/">중고거래</a></li>
-              <li class="_1knjz49n"><a class="_1knjz49o _1s38h9c4 _1s38h9c2 undefined"
-                                       href="/kr/nearby-stores/">동네업체</a></li>
-              <li class="_1knjz49n"><a class="_1knjz49o _1s38h9c4 _1s38h9c2 undefined" href="/kr/jobs/">알바</a></li>
-              <li class="_1knjz49n"><a class="_1knjz49o _1s38h9c4 _1s38h9c2 undefined" href="/kr/realty/">부동산 직거래</a>
-              </li>
-              <li class="_1knjz49n"><a class="_1knjz49o _1s38h9c4 _1s38h9c2 undefined" href="/kr/car/">중고차 직거래</a></li>
-            </ul>
-          </nav>
-          <div class="_1s38h9c1 _1s38h9c0"><span><button class="_1knjz496 _1s38h9c3 _1s38h9c0 _1s38h9c2"><svg width="24"
-                                                                                                              height="24"
-                                                                                                              viewBox="0 0 24 24"
-                                                                                                              fill="none"
-                                                                                                              xmlns="http://www.w3.org/2000/svg"><path
-              fill-rule="evenodd" clip-rule="evenodd"
-              d="M3.5999 10.5C3.5999 6.68926 6.68914 3.60002 10.4999 3.60002C14.3107 3.60002 17.3999 6.68926 17.3999 10.5C17.3999 14.3108 14.3107 17.4 10.4999 17.4C6.68914 17.4 3.5999 14.3108 3.5999 10.5ZM10.4999 2.40002C6.0264 2.40002 2.3999 6.02652 2.3999 10.5C2.3999 14.9735 6.0264 18.6 10.4999 18.6C12.5207 18.6 14.3687 17.86 15.7876 16.6362L20.5756 21.4243C20.81 21.6586 21.1899 21.6586 21.4242 21.4243C21.6585 21.19 21.6585 20.8101 21.4242 20.5758L16.6361 15.7877C17.8599 14.3688 18.5999 12.5208 18.5999 10.5C18.5999 6.02652 14.9734 2.40002 10.4999 2.40002Z"
-              fill="#212124"></path></svg></button><form novalidate="" class="_1knjz498"><input type="search"
-                                                                                                class="_1knjz49a"
-                                                                                                placeholder="물품이나 동네를 검색해보세요"
-                                                                                                value=""></form></span><span><button
-              class="_1knjz495 _1s38h9c3 _1s38h9c0 _1s38h9c2"><svg width="24" height="24" viewBox="0 0 24 24"
-                                                                   fill="none" xmlns="http://www.w3.org/2000/svg"><path
-              fill-rule="evenodd" clip-rule="evenodd"
-              d="M3 5.00002C3 4.66865 3.26863 4.40002 3.6 4.40002H20.4C20.7314 4.40002 21 4.66865 21 5.00002C21 5.3314 20.7314 5.60002 20.4 5.60002H3.6C3.26863 5.60002 3 5.3314 3 5.00002Z"
-              fill="#212124"></path><path fill-rule="evenodd" clip-rule="evenodd"
-                                          d="M3 12C3 11.6687 3.26863 11.4 3.6 11.4H20.4C20.7314 11.4 21 11.6687 21 12C21 12.3314 20.7314 12.6 20.4 12.6H3.6C3.26863 12.6 3 12.3314 3 12Z"
-                                          fill="#212124"></path><path fill-rule="evenodd" clip-rule="evenodd"
-                                                                      d="M3 19C3 18.6687 3.26863 18.4 3.6 18.4H20.4C20.7314 18.4 21 18.6687 21 19C21 19.3314 20.7314 19.6 20.4 19.6H3.6C3.26863 19.6 3 19.3314 3 19Z"
-                                                                      fill="#212124"></path></svg></button><button
-              type="button"
-              class="karrot-button r14vym0 _1s38h9c3 _1s38h9c4 r14vym4 r14vym7 _1knjz49i">채팅하기</button></span></div>
-        </div>
-      </div>
+      <Header/>
     </div>
   </div>
-
   <main>
     <section id="rebranded-home-main-section-top" class="background-pale-peach">
       <div id="home-main-top">
@@ -81,7 +40,8 @@
               <img class="fixed-apple-store" alt="App Store"
                    src="https://d1unjqcospf8gs.cloudfront.net/assets/home/base/header/apple-store-white-9ebb10b431c549dd19f032f70e6762df307939b5add030265d9c2dea888b2d03.svg">
               <div class="fixed-download-text">App Store</div>
-            </a></div>
+            </a>
+          </div>
         </div>
         <div class="home-main-image-top">
           <img
@@ -91,107 +51,37 @@
         </div>
       </div>
     </section>
-    <!--    <section class="rebranded-home-main-section" style="background-color:#f9f9f9">
-          <div class="home-main-content">
-            <div class="home-main-image home-main-image-01">
-              <img srcset="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/rebranded-image-1-c84dd43a3a50742bc296bd36e8b9b1c4a487b8d36aaf67e7f90ecce80201cf60.webp " class="home-main-image-01" alt="우리동네 중고 직거래 사진" src="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/rebranded-image-1-efd09cb54044140d530c6647906303c669ca80a8b51473722fc2b1747a3d9047.png">
-            </div>
-            <div>
-              <p class="service-title">
-                중고거래
-              </p>
-              <h1 class="home-main-title">믿을만한<br>이웃 간 중고거래</h1>
-              <p class="service-desc">
-                동네 주민들과 가깝고 따뜻한 거래를<br>지금 경험해보세요.
-              </p>
-              <div class="home-buttons">
-                <a class="home-button button-text" href="/hot_articles">인기매물 보기</a>
-                <a class="home-button button-text" href="/trust">믿을 수 있는 중고거래</a>
-              </div>
-            </div>
+
+    <section class="rebranded-home-main-section" style="background-color:#f9f9f9">
+      <div class="home-main-content">
+        <div class="home-main-image home-main-image-01">
+          <img
+              srcset="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/rebranded-image-1-c84dd43a3a50742bc296bd36e8b9b1c4a487b8d36aaf67e7f90ecce80201cf60.webp "
+              class="home-main-image-01" alt="우리동네 중고 직거래 사진"
+              src="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/rebranded-image-1-efd09cb54044140d530c6647906303c669ca80a8b51473722fc2b1747a3d9047.png">
+        </div>
+        <div>
+          <p class="service-title">
+            중고거래
+          </p>
+          <h1 class="home-main-title">믿을만한<br>이웃 간 중고거래</h1>
+          <p class="service-desc">
+            동네 주민들과 가깝고 따뜻한 거래를<br>지금 경험해보세요.
+          </p>
+          <div class="home-buttons">
+            <a class="home-button button-text" href="/hot_articles">인기매물 보기</a>
+            <a class="home-button button-text" href="/trust">믿을 수 있는 중고거래</a>
           </div>
-        </section>
-        <section class="rebranded-home-main-section" style="background-color:#FFF8F1;">
-          <div class="home-main-content home-main-reverse">
-            <div class="home-main-image home-main-image-02">
-              <img srcset="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/rebranded-image-2-78a84106155b4b2c341b6425515a90782dc08a6f443ba0bb622700ef788d2e19.webp " class="home-main-image-02" loading="lazy" alt="이웃과 함께하는 동네생활" src="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/rebranded-image-2-c99a6862a786b8f801c08ea8e76cfc0c06b5f86442c90ff7bffef0f99146321e.png">
-            </div>
-            <div>
-              <p class="service-title">
-                동네생활
-              </p>
-              <h1 class="home-main-title">이웃만 아는<br>동네 정보와 이야기</h1>
-              <p class="service-desc">
-                우리동네의 다양한 정보와 이야기를<br>공감과 댓글로 나누어요.
-              </p>
-              <ul class="home-story-list">
-                <li class="home-story-list-item">
-                  <div class="icon-story icon-story-01"></div>
-                  <div class="text-s text-bold mt-3 mb-2">동네모임</div>
-                  <div class="item-desc">근처 이웃들과 동네<br>이야기를 해보세요.</div>
-                </li>
-                <li class="home-story-list-item">
-                  <div class="icon-story icon-story-02"></div>
-                  <div class="text-s text-bold mt-3 mb-2">동네질문</div>
-                  <div class="item-desc">궁금한 게 있을 땐<br>이웃에 물어보세요.</div>
-                </li>
-                <li class="home-story-list-item">
-                  <div class="icon-story icon-story-03"></div>
-                  <div class="text-s text-bold mt-3 mb-2">동네분실센터</div>
-                  <div class="item-desc">무언가를 잃어버렸다면<br>글을 올려보세요.</div>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </section>
-        <section class="rebranded-home-main-section" style="background-color:#f9f9f9;">
-          <div class="home-main-content">
-            <div class="home-main-image home-main-image-03">
-              <img srcset="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/rebranded-image-3-fa126bf72868e1a60f6a67dde54d8a39d51e70ee5df415ebf0ab3175e23d8ee1.webp " class="home-main-image-03" loading="lazy" alt="당근하듯 쉽고 편한 동네 알바 구하기" src="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/rebranded-image-3-c105057eecd5aecd8343a3ad6b396e64a79ee75d54b3e6239eb231779ab0db04.png">
-            </div>
-            <div>
-              <p class="service-title">
-                알바
-              </p>
-              <h1 class="home-main-title">걸어서 10분!<br>동네 알바 구하기</h1>
-              <p class="service-desc">
-                당근하듯 쉽고, 편하게<br>당근 알바로 동네 알바를 구할 수 있어요.
-              </p>
-              <div class="home-buttons">
-                <a class="home-button button-text" href="https://daangn.com/kr/jobs/">내근처 알바 보기</a>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section class="rebranded-home-main-section" style="background-color:#F6FDEC;">
-          <div class="home-main-content home-main-reverse">
-            <div class="home-main-image home-main-image-04">
-              <img srcset="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/image-business-cc45c805de4a639e259aea606640042d09bccf358e8d20f50af325dd994aceef.webp " class="home-main-image-04" loading="lazy" alt="내 근처에서 찾는 동네업체" src="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/image-business-cc45c805de4a639e259aea606640042d09bccf358e8d20f50af325dd994aceef.webp">
-            </div>
-            <div>
-              <p class="service-title">
-                동네업체
-              </p>
-              <h1 class="home-main-title">내 근처에서 찾는<br>동네업체</h1>
-              <p class="service-desc">
-                이웃들의 추천 후기를 확인하고<br>동네 곳곳의 업체들을 찾을 수 있어요.
-              </p>
-              <div class="home-buttons">
-                <a target="_blank" id="town-link-button" class="ga-click home-button button-text" data-event-category="town_link_from" data-event-action="index_base" data-event-label="main_town" href="https://town.daangn.com">당근 동네업체 보기</a>
-              </div>
-            </div>
-          </div>
-        </section>-->
+        </div>
+      </div>
+    </section>
   </main>
+  <div id="footer-root">
+    <div class="light-theme">
+      <Footer/>
+    </div>
+  </div>
 </template>
-
-<script>
-
-export default {
-  name: 'App',
-  components: {}
-}
-</script>
 
 <style>
 @import "./components/css/app.css";
