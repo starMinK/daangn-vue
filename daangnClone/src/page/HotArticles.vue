797036b0c1edf061037fdd2af2168e169aa963a6
starMinK <rbals040329@gmail.com>
2023-12-18 08:49:16 +0900

    20231218

---
+++ daangnClone/src/page/HotArticles.vue
@@ -0,0 +1,92 @@
+<script setup>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: HotArticles,
+  components: {
+    Header,
+    Footer,
+  },
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<template>
+  <Header></Header>
+  <section class="fleamarket-cover">
+    <div class="cover-content">
+      <h1 class="cover-title">
+        믿을만한
+        <br/>
+        이웃 간 중고거래
+      </h1>
+      <span class="cover-description">
+        동네 주민들과 가깝고 따뜩한 거개를
+        <br/>
+        지금 경험해보세요.
+      </span>
+
+      <div class="cover-image">
+        <span class="fleamarket-cover-image">
+          <img
+              srcset="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/fleamarket-39d1db152a4769a6071f587fa9320b254085d726a06f61d544728b9ab3bd940a.webp"
+              alt="믿을만한 이웃 간 중고거래"
+              src="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/fleamarket-39d1db152a4769a6071f587fa9320b254085d726a06f61d544728b9ab3bd940a.webp"/>
+        </span>
+      </div>
+    </div>
+  </section>
+
+  <section id="fleamarket-article-list">
+    <h1 class="text-center article-list-title"> 중고거래 인기매물</h1>
+    <div class="cards-wrap">
+      <article class="card-top" v-for="card in cards" :key="card">
+        <!--card.href = "/articles/[id]"-->
+        <a class="card-link" href="{{ card.href }}">
+          <div class="card-photo">
+            <img alt="{{ card.name }}" src="{{ card.imgUrl }}">
+          </div>
+          <div class="card-desc">
+            <h2 class="card-title">{{ card.name }}</h2>
+            <!--card.price = 190,000원-->
+            <div class="card-price">{{ card.price }}</div>
+            <!--card.regionName = 경기도 성남시 분당구 정자동-->
+            <div class="card-region-name">{{ card.regionName }}</div>
+            <div class="card-counts">
+              <span> 관심 {{ card.likeCount }}</span>
+
+              ∙
+
+              <span> 채팅 {{ card.chatCount }}</span>
+            </div>
+          </div>
+        </a>
+      </article>
+    </div>
+
+    <div class="text-center article-list-more">
+      <a id="hot-articles-more" data-url="/hot_artivles">인기매물 더 보기</a>
+    </div>
+  </section>
+
+  <section class="fleamarket-keywords">
+    <h3 class="keywords-list">
+      <a href="/top_keywords">중고거래 인기검색어</a>
+    </h3>
+    <ul class="keywords-list">
+      <li class="keyword-item" v-for="keyword in keywords" :key="keyword">
+        <!--keyword.href = /search/%EC%88%9C%EA%B8%`88`-->
+        <a class="keyword-link" href="{{ keyword.href }}">{{ keyword.name }}</a>
+      </li>
+    </ul>
+  </section>
+  <Footer></Footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ daangnClone/src/page/Main.vue
@@ -0,0 +1,85 @@
+<script>
+import Header from "../components/Header.vue"
+import Footer from "../components/Footer.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+
+  }
+}
+</script>
+
+<template>
+  <Header></Header>
+  <main>
+    <!--섹션 1-->
+    <section id="rebranded-home-main-section-top" class="background-pale-peach">
+      <div id="home-main-top">
+        <div class="home-main-desc">
+          <h1 class="home-main-title">당신 근처의<br>지역 생활 커뮤니티</h1>
+          <p class="service-desc">
+            동네라서 가능한 모든 것<br>당근에서 가까운 이웃과 함께해요.
+          </p>
+          <br>
+          <div id="download-buttons">
+            <!--구글 스토어 아이콘-->
+            <a target="_blank" class="fixed-download-button" id="header-download-button-android"
+               href="https://play.google.com/store/apps/details?id=com.towneers.www">
+              <img class="fixed-google-play" alt="Google Play"
+                   src="https://d1unjqcospf8gs.cloudfront.net/assets/home/base/header/google-play-white-0531cab5dbe15262e226cfb4acebeb316708ae0034d50b86ad4d809a03b6f5f0.svg">
+              <div class="fixed-download-text">Google Play</div>
+            </a>
+            <!--앱 스토어 아이콘-->
+            <a target="_blank" class="fixed-download-button" id="header-download-button-ios"
+               href="https://apps.apple.com/app/id1018769995">
+              <img class="fixed-apple-store" alt="App Store"
+                   src="https://d1unjqcospf8gs.cloudfront.net/assets/home/base/header/apple-store-white-9ebb10b431c549dd19f032f70e6762df307939b5add030265d9c2dea888b2d03.svg">
+              <div class="fixed-download-text">App Store</div>
+            </a>
+          </div>
+        </div>
+
+        <!--당근이 이미지-->
+        <div class="home-main-image-top">
+          <img
+              srcset="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/rebranded-image-top-e765d561ee9df7f5ab897f622b8b5a35aaa70314f734e097ea70e6c83bdd73f1.webp "
+              class="home-main-image-top" alt="중고거래, 동네생활 질문글, 알바와 동네업체"
+              src="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/rebranded-image-top-eb44f81acb1938b57ba029196887cdd56fbb66dc46aa5d8c6d8392a7d8c9e671.png">
+        </div>
+      </div>
+    </section>
+
+    <!--섹션 2-->
+    <section class="rebranded-home-main-section" style="background-color:#f9f9f9">
+      <div class="home-main-content">
+        <div class="home-main-image home-main-image-01">
+          <img
+              srcset="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/rebranded-image-1-c84dd43a3a50742bc296bd36e8b9b1c4a487b8d36aaf67e7f90ecce80201cf60.webp "
+              class="home-main-image-01" alt="우리동네 중고 직거래 사진"
+              src="https://d1unjqcospf8gs.cloudfront.net/assets/home/main/3x/rebranded-image-1-efd09cb54044140d530c6647906303c669ca80a8b51473722fc2b1747a3d9047.png">
+        </div>
+        <div>
+          <p class="service-title">
+            중고거래
+          </p>
+          <h1 class="home-main-title">믿을만한<br>이웃 간 중고거래</h1>
+          <p class="service-desc">
+            동네 주민들과 가깝고 따뜻한 거래를<br>지금 경험해보세요.
+          </p>
+          <div class="home-buttons">
+            <a class="home-button button-text" href="/hot_articles">인기매물 보기</a>
+            <a class="home-button button-text" href="/trust">믿을 수 있는 중고거래</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  <Footer></Footer>
+</template>
+
+<style>
+@import "../components/css/app.css";
+</style>
